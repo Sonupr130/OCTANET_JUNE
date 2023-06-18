c9bae971af7c6d0b16a361c90d51c45e035c1850
Sonu Pradhan <83033993+Sonupr130@users.noreply.github.com>
2023-06-18 22:51:08 +0530

    Third task complete

---
+++ Disney+Hotstar/slider-data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'WI  vs IND 317/8 (50)',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/slider2.webp'
+    },
+    {
+        name: 'Rudra: The Edge of Darkness',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/slider1.webp'
+    },
+    {
+        name: 'The Book of Boba Fett',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/slider3.webp'
+    },
+    {
+        name: 'raya and the last dragon',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/slider4.PNG'
+    },
+    {
+        name: 'luca',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'img/slider5.PNG'
+    }
+]
